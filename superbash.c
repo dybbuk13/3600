cdfd7037d8061913694aa8ce111c18d4d3a4cf73
tj6292 <tj6292@gmail.com>
2014-09-30 08:34:57 -0500

    Update superbash.c
    Opened the files and stuff, if you have questions, let me know.

--- superbash.c
+++ superbash.c
@@ -5,10 +5,28 @@
 **Super BASH
 */
 
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main () {
+FILE* pbashfile ;
+FILE* psuperbash ;
 
+pbashfile = fopen("file", "r") ;
+if (pbashfile==NULL){
+   printf ("Error reading file.\n\n") ;
+   return 0 ;
+}
+psuperbash = fopen("newfile", "w") ;
+if (psuperbash==NULL){
+   printf ("Error creating file.\n\n") ;
+   return 0 ;
+}
 
+//okay the files are "open" now.
+//to read from the file use fscanf(pbashfile, *normalscanfstuff)
+//write is the same fprintf(psuperbash, *normalprintfstuff)
 
    return 0 ;
 }
