181335e8a70c14d26107ae1f28bba841126fcbdd
tj6292 <tj6292@gmail.com>
2014-10-02 09:45:02 -0500

    Update superbash.c

--- superbash.c
+++ superbash.c
@@ -8,10 +8,15 @@
 #include <stdio.h>
 #include <string.h>
 
+void fixequal (char bashline[]) ;
+void fixifthen (char bashline[]) ;
+void fixloop (char bashline[]) ;
+
 int main (int argc, char** argv) {
 FILE* pbashfile ;
 FILE* psuperbash ;
 char bashline[60] ;
+char* test1, test2 ;
 
 pbashfile = fopen(argv[1], "r") ;
 if (pbashfile==NULL){
@@ -29,10 +34,32 @@ if (psuperbash==NULL){
 //we should be able to use sscanf to parse this string and make tests in the while loop...
 //the loop case is the only challenge now :)
 while (fgets(bashline, 60, pbashfile)) {
+   test1 = strchr(bashline, '=') ;
+   if (test1 != NULL) {
+      fixequal(bashline) ;    
+   } 
+   test1 = strstr(bashline, "if")
+   test2 = strstr(bashline, "then") ;
+   if (test1!=NULL && test2!=NULL){
+      fixifthen(bashline) ;
+   }
    fprintf (psuperbash, "%s", bashline) ;
 }
 
    fclose (pbashfile) ;
    fclose (psuperbash) ;
    return 0 ;
+}
+
+void fixequal (char bashline[]) {
+   
+return ;
+}
+void fixifthen (char bashline[]) {
+   
+return ;   
+}
+void fixloop (char bashline[]) {
+   
+return ;   
 }
